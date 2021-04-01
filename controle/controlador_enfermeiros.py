2d81a49b99253eaec3f4a2639949f0239007774b
Erich Hanemann Jr <erich_juninho@hotmail.com>
2021-04-01 01:31:49 -0300

    pequenos ajustes

--- controle/controlador_enfermeiros.py
+++ controle/controlador_enfermeiros.py
@@ -19,20 +19,37 @@ class ControladorEnfermeiros():
         self.__enfermeiros.append(enfermeiro)
 
     def editar_enfermeiro(self):
-        pass
+        enfermeiro_editar = self.get_enfermeiro()
+        dados_editar = self.__tela_enfermeiros.pegar_dados_enfermeiro_edicao()
+        for enfermeiro in self.__enfermeiros:
+            if enfermeiro == enfermeiro_editar:
+                enfermeiro.nome = dados_editar['nome']
+                enfermeiro.matricula = dados_editar['matricula']
+                enfermeiro.status = "Ativo"
+
+    def consultar_enfermeiro(self):
+        enfermeiro_consultar = self.get_enfermeiro()
+        for enfermeiro in self.__enfermeiros:
+            if enfermeiro == enfermeiro_consultar:
+                self.__tela_enfermeiros.mostrar_enfermeiro(
+                    {"nome": enfermeiro.nome,
+                    "cpf": enfermeiro.cpf,
+                    "matricula": enfermeiro.matricula,
+                    "status": enfermeiro.status}
+                    )
 
     def get_enfermeiro(self):
         matricula = self.__tela_enfermeiros.selecionar_enfermeiro()
         for enfermeiro in self.__enfermeiros:
             if matricula == enfermeiro.matricula:
                 salvar_enfermeiro = enfermeiro
-        return salvar_enfermeiro
+                return salvar_enfermeiro
 
     def remover_enfermeiro(self):
         enfermeiro_editar = self.get_enfermeiro()
         for enfermeiro in self.__enfermeiros:
             if enfermeiro == enfermeiro_editar:
-                enfermeiro.status(Inativo)
+                enfermeiro.status = "Inativo"
 
     def listar_enfermeiros(self):
         for enfermeiro in self.__enfermeiros:
@@ -52,7 +69,7 @@ class ControladorEnfermeiros():
     def abre_tela(self):
         lista_opcoes = {1: self.cadastrar_enfermeiro,
                         2: self.editar_enfermeiro,
-                        3: self.get_enfermeiro,
+                        3: self.consultar_enfermeiro,
                         4: self.remover_enfermeiro,
                         5: self.listar_enfermeiros,
                         6: self.listar_pacientes_por_enfermeiro,
