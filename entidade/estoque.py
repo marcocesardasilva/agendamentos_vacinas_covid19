aeda074a21a610fb5c0c0a876329bfb863fd4d26
Marco Cesar da Silva <marcocesardasilva@gmail.com>
2021-04-27 08:02:51 -0300

    Criadas as persistências e a classe estoque.

---
+++ entidade/estoque.py
@@ -0,0 +1,5 @@
+'''
+Classe criada apenas para sanar a falta de agregação no trabalho.
+Fazer aqui um estoque, que se refere ao local físico onde a vacina está armazenada.
+Alterar no cadastro da Vacina para adicionar este estoque.
+'''
